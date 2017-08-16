d43f6552d201156a2f6419a77760de95405d38d3
Radar <radar@protonmail.com>
2017-08-16 11:22:33 +0200

    Add readme

--- init.sh
+++ init.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-repo="laptop-provisioning"
-
 sudo apt-get update
 sudo apt-get install -y python-pip
 pip install --user pip setuptools
 pip install --user ansible
-curl -sL https://github.com/radar-aol-pl/$repo/archive/master.tar.gz | tar zx --transform 's/-master//'
-cd $repo
+curl -sL https://github.com/radar-aol-pl/laptop-provisioning/archive/master.tar.gz | tar zx --transform 's/-master//'
